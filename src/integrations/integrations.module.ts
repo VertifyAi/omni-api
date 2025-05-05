1a800aa3ec36f0cf6d19e32ae0b4d8c2d0511422
joa7b <joa7@protonmail.com>
2025-05-05 15:16:28 -0300

    feat: :rocket:

--- src/integrations/integrations.module.ts
+++ src/integrations/integrations.module.ts
@@ -5,9 +5,15 @@ import { IntegrationsService } from './integrations.service';
 import { Integration } from './entities/integration.entity';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Integration]), JwtModule, ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([Integration]),
+    JwtModule,
+    ConfigModule,
+    HttpModule,
+  ],
   controllers: [IntegrationsController],
   providers: [IntegrationsService],
 })
--- src/integrations/integrations.service.ts
+++ src/integrations/integrations.service.ts
@@ -4,19 +4,22 @@ import { Repository } from 'typeorm';
 import { Integration, IntegrationType } from './entities/integration.entity';
 import { User } from 'src/users/entities/user.entity';
 import { WhatsappIntegrationDto } from './dto/whatsapp-integration.dto';
+import { HttpService } from '@nestjs/axios';
+// import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class IntegrationsService {
   constructor(
     @InjectRepository(Integration)
     private readonly integrationRepository: Repository<Integration>,
+    private readonly httpService: HttpService,
   ) {}
 
   async whatsappIntegration(
     currentUser: User,
     whatsappIntegrationDto: WhatsappIntegrationDto,
   ): Promise<Integration> {
-    const { config } = whatsappIntegrationDto;
+    console.log('whatsappIntegrationDto', whatsappIntegrationDto);
     let integration = await this.integrationRepository.findOneBy({
       companyId: currentUser.companyId,
       type: IntegrationType.WHATSAPP,
@@ -30,9 +33,42 @@ export class IntegrationsService {
       companyId: currentUser.companyId,
       type: IntegrationType.WHATSAPP,
       active: true,
-      config,
+      config: JSON.stringify(whatsappIntegrationDto),
     });
     await this.integrationRepository.save(integration);
     return integration;
+  }
+
+  async findAllIntegrations(currentUser: User): Promise<Integration[]> {
+    return await this.integrationRepository.find({
+      where: { companyId: currentUser.companyId, active: true },
+    });
+  }
+
+  async getWhatsappPhoneNumbers(currentUser: User) {
+    const integration = await this.integrationRepository.findOneBy({
+      companyId: currentUser.companyId,
+      type: IntegrationType.WHATSAPP,
+    });
+
+    if (!integration) {
+      throw new Error('Integration not found');
+    }
+
+    const config = JSON.parse(integration.config);
+    console.log('config', config);
+
+    // await lastValueFrom(
+    //   this.httpService.post(
+    //     `https://graph.facebook.com/v19.0/${process.env.META_PHONE_NUMBER_ID}/messages`,
+    //     whatsappPayload,
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${process.env.META_ACESS_TOKEN}`,
+    //         'Content-Type': 'application/json',
+    //       },
+    //     },
+    //   ),
+    // );
   }
 }
