51e3fed6d636f24e2daae8cd2c751326d89340cc
joa7b <joa7@protonmail.com>
2025-05-05 08:45:38 -0300

    feat: find all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +10,11 @@ export class UsersController {
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.create(createUserDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get()
+  async findAll(@Request() req) {
+    return await this.usersService.findAll(req.user.companyId);
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), JwtModule, ConfigModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
