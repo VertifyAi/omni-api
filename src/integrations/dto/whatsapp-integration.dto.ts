35a7074571af955d9dfff72031d90d7a188ba680
joa7b <joa7@protonmail.com>
2025-07-14 16:06:27 -0300

    fix: waba integration

--- src/integrations/dto/whatsapp-integration.dto.ts
+++ src/integrations/dto/whatsapp-integration.dto.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class WhatsappIntegrationDto {
   @IsString()
@@ -6,8 +6,8 @@ export class WhatsappIntegrationDto {
   access_token: string;
 
   @IsString()
-  @IsNotEmpty()
-  data_access_expiration_time: string;
+  @IsOptional()
+  data_access_expiration_time?: string;
 
   @IsString()
   @IsNotEmpty()
